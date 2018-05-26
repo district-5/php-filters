58ae932abdf912c43c23a1ef4735e623f3f309af
Mark Morgan <mark.morgan@district5.co.uk>
2018-05-26 14:48:12 +0100

    added array string versions of trim, upper and lower

---
+++ src/Filter/ArrayOfStringLower.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * District5 - Filters
+ *
+ * @copyright District5
+ *
+ * @author District5 Digital
+ * @link https://www.district5.co.uk
+ *
+ * @license This software and associated documentation (the "Software") may not be
+ * used, copied, modified, distributed, published or licensed to any 3rd party
+ * without the written permission of District5 or its author.
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all licensed copies of the Software.
+ */
+namespace District5\Filter;
+
+/**
+ * ArrayOfStringLower
+ * 
+ * An array of strings filter to make all strings lower case
+ * 
+ * @author Mark Morgan <mark.morgan@district5.co.uk>
+ */
+class ArrayOfStringLower extends StringLower
+{
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see \District5\Filter\I::filter()
+	 */
+	public function filter($value)
+	{
+	    $toReturn = array();
+
+	    foreach ($value as $preFiltered)
+        {
+            $toReturn[] = parent::filter($preFiltered);
+        }
+
+		return $toReturn;
+	}
+}
