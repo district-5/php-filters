c567f94fdd95164687436a4cecd9e6be34ad254b
Mark Morgan <mark.morgan@district5.co.uk>
2018-05-26 15:30:08 +0100

    added filter to create an array of unique strings

---
+++ src/Filter/ArrayOfStringUnique.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * District5 - Filters
+ *
+ * @copyright District5
+ *
+ * @author District5 Digital
+ * @link https://www.district5.co.uk
+ *
+ * @license This software and associated documentation (the "Software") may not be
+ * used, copied, modified, distributed, published or licensed to any 3rd party
+ * without the written permission of District5 or its author.
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all licensed copies of the Software.
+ */
+namespace District5\Filter;
+
+/**
+ * ArrayOfStringUnique
+ * 
+ * An array of unique strings
+ * 
+ * @author Mark Morgan <mark.morgan@district5.co.uk>
+ */
+class ArrayOfStringUnique extends StringUpper
+{
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see \District5\Filter\I::filter()
+	 */
+	public function filter($value)
+	{
+	    return array_unique($value);
+	}
+}
