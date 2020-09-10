440f1949020097d911563d907138c952350d3f78
Mark Morgan <mark.morgan@district5.co.uk>
2020-09-10 10:52:22 +0100

    added filter for converting array of strings to array of integers

---
+++ src/Filter/ArrayOfStringToInt.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * District5 - Filters
+ *
+ * @copyright District5
+ *
+ * @author District5 Digital
+ * @link https://www.district5.co.uk
+ *
+ * @license This software and associated documentation (the "Software") may not be
+ * used, copied, modified, distributed, published or licensed to any 3rd party
+ * without the written permission of District5 or its author.
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all licensed copies of the Software.
+ */
+namespace District5\Filter;
+
+/**
+ * ArrayOfStringToInt
+ * 
+ * An array of strings filter to make all strings integers.
+ *
+ * NOTE: this should use validation first to validate that all values are a string representation of an integer,
+ * otherwise unexpected behaviour may occur as this function depends on PHP's intval function
+ * 
+ * @author Mark Morgan <mark.morgan@district5.co.uk>
+ */
+class ArrayOfStringToInt extends StringTrim
+{
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see \District5\Filter\I::filter()
+	 */
+	public function filter($value)
+	{
+	    $toReturn = array();
+
+	    foreach ($value as $preFiltered)
+        {
+            $toReturn[] = intval($preFiltered);
+        }
+
+		return $toReturn;
+	}
+}
